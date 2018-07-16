2660faabbc07276ddc684799a6edb4d9d348c0ee
claudenm <claude.mendelson@gmail.com>
2018-07-16 11:21:14 -0400

    add additional r53 methods to gem

--- lib/movable_ink/aws/route53.rb
+++ lib/movable_ink/aws/route53.rb
@@ -10,6 +10,23 @@ module MovableInk
         @resource_record_sets[hosted_zone_id] ||= list_all_r53_resource_record_sets(hosted_zone_id)
       end
 
+      def get_resource_record_sets_by_instance_name(zone, instance_name)
+        resource_record_sets(zone).select{|rrs| rrs.set_identifier == instance_name}.first.to_h
+      end
+
+      def delete_resource_record_sets(zone, instance_name)
+        change_batch = {
+          "changes": [{
+            "action": 'DELETE',
+            "resource_record_set": get_resource_record_sets_by_instance_name(zone, instance_name)
+          }]
+        }
+
+        run_with_backoff do
+          route53.change_resource_record_sets({change_batch: change_batch, hosted_zone_id: zone})
+        end
+      end
+
       def list_all_r53_resource_record_sets(hosted_zone_id)
         run_with_backoff do
           route53.list_resource_record_sets({
