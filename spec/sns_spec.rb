dbd1d862a8301df93adb494c109f79a7b65d6dae
aaronackerman <aackerman@movableink.com>
2019-03-28 14:37:01 -0500

    [ch21453] Send a message to sns for pagerduty when nsq is unable to be drained from an instance

--- spec/sns_spec.rb
+++ spec/sns_spec.rb
@@ -6,6 +6,9 @@ describe MovableInk::AWS::SNS do
   let(:topic_data) { sns.stub_data(:list_topics, topics: [
       {
         topic_arn: 'slack-aws-alerts'
+      },
+      {
+        topic_arn: 'pagerduty-custom-alerts'
       }
     ])
   }
@@ -19,6 +22,14 @@ describe MovableInk::AWS::SNS do
     expect(aws.sns_slack_topic_arn).to eq('slack-aws-alerts')
   end
 
+  it 'should find the pagerduty slack sns topic' do
+    sns.stub_responses(:list_topics, topic_data)
+    allow(aws).to receive(:my_region).and_return('us-east-1')
+    allow(aws).to receive(:sns).and_return(sns)
+
+    expect(aws.sns_pagerduty_topic_arn).to eq('pagerduty-custom-alerts')
+  end
+
   it "should notify with the specified subject and message" do
     sns.stub_responses(:list_topics, topic_data)
     allow(aws).to receive(:my_region).and_return('us-east-1')
@@ -26,7 +37,15 @@ describe MovableInk::AWS::SNS do
     allow(aws).to receive(:sns).and_return(sns)
 
     expect(aws.notify_slack(subject: 'Test subject', message: 'Test message').message_id).to eq('messageId')
+  end
 
+  it 'should notify pagerduty with related information' do
+    sns.stub_responses(:list_topics, topic_data)
+    allow(aws).to receive(:my_region).and_return('us-east-1')
+    allow(aws).to receive(:instance_id).and_return('test instance')
+    allow(aws).to receive(:sns).and_return(sns)
+
+    expect(aws.notify_pagerduty(region: 'us-east-1', instance_id: 'i-987654321').message_id).to eq('messageId')
   end
 
   it "should truncate subjects longer than 100 characters" do
@@ -41,6 +60,5 @@ describe MovableInk::AWS::SNS do
     expect(sns).to receive(:publish).with({:topic_arn=>"slack-aws-alerts", :message=>"Test message", :subject => expected_subject})
 
     aws.notify_slack(subject: subject, message: message)
-
   end
 end
