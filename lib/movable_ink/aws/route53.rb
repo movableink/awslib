18debaa55dc1b7560cd63614ba6b95c6a4343fd0
Artem Nosulchyk <anosulchyk@movableink.com>
2022-04-04 15:13:23 -0400

    Support of route53 client override.

--- lib/movable_ink/aws/route53.rb
+++ lib/movable_ink/aws/route53.rb
@@ -3,21 +3,21 @@ require 'aws-sdk-route53'
 module MovableInk
   class AWS
     module Route53
-      def route53
-        @route53_client ||= Aws::Route53::Client.new(region: 'us-east-1')
+      def route53(client = nil)
+        @route53_client ||= (client) ? client : Aws::Route53::Client.new(region: 'us-east-1')
       end
 
-      def resource_record_sets(hosted_zone_id)
+      def resource_record_sets(hosted_zone_id, client = nil)
         @resource_record_sets ||= {}
-        @resource_record_sets[hosted_zone_id] ||= list_all_r53_resource_record_sets(hosted_zone_id)
+        @resource_record_sets[hosted_zone_id] ||= list_all_r53_resource_record_sets(hosted_zone_id, client)
       end
 
-      def get_resource_record_sets_by_instance_name(zone, instance_name)
-        resource_record_sets(zone).select{|rrs| rrs.set_identifier == instance_name}.first.to_h
+      def get_resource_record_sets_by_instance_name(zone, instance_name, client = nil)
+        resource_record_sets(zone, client).select{|rrs| rrs.set_identifier == instance_name}.first.to_h
       end
 
-      def delete_resource_record_sets(zone, instance_name)
-        resource_record_set = get_resource_record_sets_by_instance_name(zone, instance_name)
+      def delete_resource_record_sets(zone, instance_name, client = nil)
+        resource_record_set = get_resource_record_sets_by_instance_name(zone, instance_name, client)
         return if resource_record_set.empty?
 
         change_batch = {
@@ -28,36 +28,36 @@ module MovableInk
         }
 
         run_with_backoff do
-          route53.change_resource_record_sets({change_batch: change_batch, hosted_zone_id: zone})
+          route53(client).change_resource_record_sets({change_batch: change_batch, hosted_zone_id: zone})
         end
       end
 
-      def list_all_r53_resource_record_sets(hosted_zone_id)
+      def list_all_r53_resource_record_sets(hosted_zone_id, client = nil)
         run_with_backoff do
-          route53.list_resource_record_sets({
+          route53(client).list_resource_record_sets({
             hosted_zone_id: hosted_zone_id
           }).flat_map(&:resource_record_sets)
         end
       end
 
-      def list_health_checks
+      def list_health_checks(client = nil)
         run_with_backoff do
-          route53.list_health_checks().flat_map(&:health_checks)
+          route53(client).list_health_checks().flat_map(&:health_checks)
         end
       end
 
-      def get_health_check_tags(health_check_id)
+      def get_health_check_tags(health_check_id, client = nil)
         run_with_backoff do
-          route53.list_tags_for_resource({
+          route53(client).list_tags_for_resource({
             resource_type: 'healthcheck',
             resource_id: health_check_id
           }).resource_tag_set.tags
         end
       end
 
-      def find_health_check_by_tag(key, value)
-        list_health_checks.detect do |health_check|
-          get_health_check_tags(health_check.id).detect { |tag| tag.key == key && tag.value.include?(value) }
+      def find_health_check_by_tag(key, value, client = nil)
+        list_health_checks(client).detect do |health_check|
+          get_health_check_tags(health_check.id, client).detect { |tag| tag.key == key && tag.value.include?(value) }
         end
       end
     end
