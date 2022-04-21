e5ecc65f0c356c4f4426628e6186a1cb841b4016
Nick Doubleday <ndoubleday@movableink.com>
2022-04-21 11:51:49 -0400

    leveraging default args

--- lib/movable_ink/aws/ssm.rb
+++ lib/movable_ink/aws/ssm.rb
@@ -3,12 +3,12 @@ require 'aws-sdk-ssm'
 module MovableInk
   class AWS
     module SSM
-      def ssm_client(region = nil)
-        @ssm_client ||= (region) ? region : Aws::SSM::Client.new(region: 'us-east-1')
+      def ssm_client(region = 'us-east-1')
+        @ssm_client ||= Aws::SSM::Client.new(region: region)
       end
 
-      def ssm_client_failover(failregion = nil)
-        @ssm_client_failover ||= (failregion) ? failregion: Aws::SSM::Client.new(region: 'us-west-2')
+      def ssm_client_failover(failregion = 'us-west-2')
+        @ssm_client_failover ||= Aws::SSM::Client.new(region: failregion)
       end
 
       def run_with_backoff_and_client_fallback(&block)
